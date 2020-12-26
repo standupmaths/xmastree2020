d53a6143b22c48b457e53582530a56f31f22404f
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-26 11:56:25 +0100

    added wavesoflight function

--- wavesoflight.py
+++ wavesoflight.py
@@ -2,40 +2,56 @@ import math
 from random import randint
 
 
+class Wave:
+    def __init__(self, tree):
+        self.radius = 0
+        self.max_radius = 1000
+        self.max_intensity = 100
+        self.speed = 6
+        self.source = randint(0, len(tree.coords))
+        self.color = [randint(0, self.max_intensity), randint(0, self.max_intensity), randint(0, self.max_intensity)]
+
+    def update(self, leds, tree):
+        self.radius += self.speed
+        # wave drawing
+        for led in range(len(leds)):
+            distance_from_source = math.dist(tree.coords[led], tree.coords[self.source])
+            if self.radius - self.speed < distance_from_source <= self.radius:
+                for i in range(3):
+                    if leds[led][i] < self.color[i]:
+                        leds[led][i] = self.color[i]
+
+
 # function by pikuch
 def wavesoflight(tree):
     """Draws waves of light starting at random points"""
-    # current wave radius
-    radius = 0
-    # maximum wave radius, after which it resets
-    max_radius = 800
-    # the speed at which the wave spreads
-    wave_speed = 7
-    # maximum intensity of any one colour channel
-    max_intensity = 100
-    # wave source (led index)
-    source = randint(0, len(tree.coords))
+    # list of waves
+    waves = [Wave(tree)]
+    # color persistance (1 = lasts forever)
+    persistance = 0.99
+    # time between waves
+    wave_frequency = 60
+    timer = wave_frequency
     # led colors
     leds = [[0, 0, 0]] * len(tree.coords)
-    # wave color
-    color = [randint(0, max_intensity), randint(0, max_intensity), randint(0, max_intensity)]
-    # color persistance (1 = stays forever, 0 = instantly off
-    persistance = 0.98
 
     while True:
-        if radius > max_radius:
+        timer -= 1
+        if timer <= 0:
+            timer = wave_frequency
             # create another wave
-            radius = 0
-            source = randint(0, len(tree.coords))
-            color = [randint(0, max_intensity), randint(0, max_intensity), randint(0, max_intensity)]
+            waves.append(Wave(tree))
 
-        radius += wave_speed
+        for wave in waves:
+            wave.update(leds, tree)
 
-        # wave drawing
-        for led in range(len(tree.coords)):
-            distance_from_source = math.dist(tree.coords[led], tree.coords[source])
-            if radius - wave_speed <= distance_from_source <= radius:
-                leds[led] = color
+        # remove old waves
+        to_remove = []
+        for wave in waves:
+            if wave.radius > wave.max_radius:
+                to_remove.append(wave)
+        for wave in to_remove:
+            waves.remove(wave)
 
         # color dimming
         for led in range(len(tree.coords)):
